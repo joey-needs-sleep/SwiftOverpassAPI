9fbce3ff1ff277b7eb03c3d920362cdf22abfae7
Joseph Hassell <joseph@hassell.dev>
2024-04-08 18:08:54 -0500

    added tag

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SwiftOverpassApi",
+            name: "SwiftOverpassApi", 
             targets: ["SwiftOverpassApi"]),
     ],
     dependencies: [
